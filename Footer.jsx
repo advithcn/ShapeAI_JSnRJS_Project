dfe54c37c2c09990cfde114d2c9d72aa9aade7f2
advithcn <86250576+advithcn@users.noreply.github.com>
2021-06-26 19:39:01 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>Copyright by ShapeAI @ {new Date().getFullYear()}</p>
+        <h6>Done By: Advith C N</h6>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Info() {
+  return (
+    <>
+      <div className="note">
+        <h1>JavaScript</h1>
+        <p>A basic Web Development using Javascript</p>
+      </div>
+      <div className="note">
+        <h1>React.js</h1>
+        <p>A basic Web Development using React.js</p>
+      </div>
+    </>
+  );
+}
+
+export default Info;
